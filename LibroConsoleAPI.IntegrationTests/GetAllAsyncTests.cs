1cfe130b95bc9ca26daa1d5cb6a643c7ebf5e2d7
YordanPopov <jordanpopov190@gmail.com>
2024-12-20 11:03:09 +0200

    Add GetAllAsycn tests

--- LibroConsoleAPI.IntegrationTests/GetAllAsyncTests.cs
+++ LibroConsoleAPI.IntegrationTests/GetAllAsyncTests.cs
@@ -1,4 +1,7 @@
-﻿using LibroConsoleAPI.Business.Contracts;
+﻿using LibroConsoleAPI.Business;
+using LibroConsoleAPI.Business.Contracts;
+using LibroConsoleAPI.Data.Models;
+using LibroConsoleAPI.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,14 +26,55 @@ namespace LibroConsoleAPI.IntegrationTests.XUnit
         [Fact]
         public async Task GetAllAsync_WhenBooksExist_ShouldReturnAllBooks()
         {
-            Assert.True(true);
-        }
+            //await DatabaseSeeder.SeedDatabaseAsync(_dbContext, _bookManager);
+            var books = new Book[]
+            {
+                new Book
+                {
+                    Title = "Test Book I",
+                    Author = "John Doe",
+                    ISBN = "1234567890123",
+                    YearPublished = 2020,
+                    Genre = "Fiction",
+                    Pages = 200,
+                    Price = 19.99
+                },
+                new Book
+                {
+                    Title = "Test Book II",
+                    Author = "John Wick",
+                    ISBN = "1234567890123",
+                    YearPublished = 2021,
+                    Genre = "Action",
+                    Pages = 100,
+                    Price = 10.99
+                }
+            };
 
+            foreach (var newBook in books)
+            {
+                await _bookManager.AddAsync(newBook);
+            }
+
+            var booksInDb = await _bookManager.GetAllAsync();
+            Assert.Equal(2, booksInDb.Count());
+
+            foreach (var book in booksInDb)
+            {
+                Assert.NotNull(book.Title);
+                Assert.NotNull(book.Author);
+                Assert.NotNull(book.ISBN);
+                Assert.NotNull(book.Price);
+            }
+        }
+         
 
         [Fact]
         public async Task GetAllAsync_WhenNoBooksExist_ShouldThrowKeyNotFoundException()
         {
-            Assert.True(true);
+            var exception = await Assert.ThrowsAsync<KeyNotFoundException>(() => _bookManager.GetAllAsync());
+
+            Assert.Equal("No books found.", exception.Message);
         }
     }
 }
