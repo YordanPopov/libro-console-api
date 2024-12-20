9bfcea0a1355df5c2603a2e4441be9f4942c4691
YordanPopov <jordanpopov190@gmail.com>
2024-12-20 11:48:42 +0200

    Add DeleteBookAsync tests

--- LibroConsoleAPI.IntegrationTests/DeleteBookAsyncTests.cs
+++ LibroConsoleAPI.IntegrationTests/DeleteBookAsyncTests.cs
@@ -1,4 +1,6 @@
 ﻿using LibroConsoleAPI.Business.Contracts;
+using LibroConsoleAPI.Data.Models;
+using LibroConsoleAPI.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,25 +14,61 @@ namespace LibroConsoleAPI.IntegrationTests.XUnit
         private readonly BookManagerFixture _fixture;
         private readonly IBookManager _bookManager;
         private readonly TestLibroDbContext _dbContext;
+        Book newBook;
 
         public DeleteBookAsyncTests()
         {
             _fixture = new BookManagerFixture();
             _bookManager = _fixture.BookManager;
             _dbContext = _fixture.DbContext;
+            newBook = new Book
+            {
+                Title = "Test Book",
+                Author = "John Doe",
+                ISBN = "1234567890123",
+                YearPublished = 2021,
+                Genre = "Fiction",
+                Pages = 100,
+                Price = 19.99
+            };
         }
 
         [Fact]
         public async Task DeleteBookAsync_WithValidISBN_ShouldRemoveBookFromDb()
         {
-            Assert.True(true);
+            await _bookManager.AddAsync(newBook);
+
+            await _bookManager.DeleteAsync(newBook.ISBN);
+
+            var booksInDb = _dbContext.Books.FirstOrDefault();
+            Assert.Null(booksInDb);
+
+            var exception = await Assert.ThrowsAsync<KeyNotFoundException>(() => _bookManager.GetAllAsync());
+            Assert.Equal("No books found.", exception.Message);
         }
 
 
-        [Fact]
-        public async Task DeleteBookAsync_TryToDeleteWithNullOrWhiteSpaceISBN_ShouldThrowException()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("   ")]
+        [InlineData("")]
+        public async Task TryToDeleteBookWith_NullOrWhiteSpaceISBN_ShouldThrowException(string isbn)
         {
-            Assert.True(true);
+            await _bookManager.AddAsync(newBook);
+
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _bookManager.DeleteAsync(isbn));
+            Assert.Equal("ISBN cannot be empty.", exception.Message);
+        }
+
+        [Fact]
+        public async Task TryToDeleteBookWith_NonExistentISBN_ShouldThrowException()
+        {
+            string nonExsitentISBN = "1112223334447";
+            await _bookManager.AddAsync(newBook);
+            
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => _bookManager.DeleteAsync(nonExsitentISBN));
+
+            Assert.Equal("Value cannot be null. (Parameter 'entity')", exception.Message);
         }
     }
 }
