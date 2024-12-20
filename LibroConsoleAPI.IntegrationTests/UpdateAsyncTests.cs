0684e86cad025a1bb6e0f94f7d49cd402941d8c3
YordanPopov <jordanpopov190@gmail.com>
2024-12-20 15:35:42 +0200

    Add UpdateAsync tests

--- LibroConsoleAPI.IntegrationTests/UpdateAsyncTests.cs
+++ LibroConsoleAPI.IntegrationTests/UpdateAsyncTests.cs
@@ -1,6 +1,9 @@
 ﻿using LibroConsoleAPI.Business.Contracts;
+using LibroConsoleAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,13 +26,48 @@ namespace LibroConsoleAPI.IntegrationTests.XUnit
         [Fact]
         public async Task UpdateAsync_WithValidBook_ShouldUpdateBook()
         {
-            Assert.True(true);
-        }
+            Book newBook = new Book
+            {
+                Title = "Test Book",
+                Author = "John Doe",
+                ISBN = "1234567890123",
+                YearPublished = 2021,
+                Genre = "Fiction",
+                Pages = 100,
+                Price = 19.99
+            };
+
+            await _bookManager.AddAsync(newBook);
+
+            string updatedTitle = "Test Book UPDATED";
+            newBook.Title = updatedTitle;
+
+            await _bookManager.UpdateAsync(newBook);
+            var book = await _bookManager.GetSpecificAsync(newBook.ISBN);
+            Assert.Equal(updatedTitle, book.Title);    
+        }    
 
         [Fact]
         public async Task UpdateAsync_WithInvalidBook_ShouldThrowValidationException()
         {
-            Assert.True(true);
+            Book newBook = new Book
+            {
+                Title = "Test Book",
+                Author = "John Doe",
+                ISBN = "1234567890123",
+                YearPublished = 2021,
+                Genre = "Fiction",
+                Pages = 100,
+                Price = 19.99
+            };
+
+            await _bookManager.AddAsync(newBook);
+
+            string invalidTitle = string.Empty;
+            newBook.Title = invalidTitle;
+
+            var exception = await Assert.ThrowsAsync<ValidationException>(() => _bookManager.UpdateAsync(newBook));
+            Assert.Equal("Book is invalid.", exception.Message);
         }
     }
 }
